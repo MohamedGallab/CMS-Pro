dcfccbd76c96f05361b5d733c41389d6620c5c5a
Mohamed Gallab <mohamed.w.gallab@gmail.com>
2022-02-07 18:07:42 +0300

    clean up + UI work

--- background.js
+++ background.js
@@ -1,8 +1,5 @@
 // background.js
 
-let color = "#3aa757";
-
+//change from click to page loaded ?
 chrome.runtime.onInstalled.addListener(() => {
-    chrome.storage.sync.set({ color });
-    console.log("Default background color set to %cgreen", `color: ${color}`);
 });
--- popup.js
+++ popup.js
@@ -1,12 +1,12 @@
 // Initialize button with user's preferred color
-let changeColor = document.getElementById("changeColor");
+let changeName = document.getElementById("changeName");
 
 chrome.storage.sync.get("color", ({ color }) => {
-    changeColor.style.backgroundColor = color;
+    changeName.style.backgroundColor = color;
 });
 
 // When the button is clicked, inject setPageBackgroundColor into current page
-changeColor.addEventListener("click", async () => {
+changeName.addEventListener("click", async () => {
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
     chrome.scripting.executeScript({

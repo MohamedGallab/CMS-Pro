3f952786d206fb60bf53c26fee0161dcda5f0161
Mohamed Gallab <mohamed.w.gallab@gmail.com>
2022-02-07 21:38:59 +0300

    Runs in the background now

--- background.js
+++ background.js
@@ -1,5 +1,18 @@
-// background.js
+// checks if tabs got updated and excutes once it is
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    if (changeInfo.status == 'complete') {
+        chrome.scripting.executeScript({
+            target: { tabId: tab.id },
+            function: addDownloadAttribute
+        });
+    }
+})
 
-//change from click to page loaded ?
-chrome.runtime.onInstalled.addListener(() => {
-});
+// The body of this function will be executed as a content script inside the CMS pages
+function addDownloadAttribute() {
+    let downloadButtons = document.querySelectorAll('[id=download]');
+    let buttonNames = document.querySelectorAll('[class=card-body] div strong');
+    for (let index = 0; index < downloadButtons.length; index++) {
+        downloadButtons[index].setAttribute("download", buttonNames[index].innerHTML.substring(4).trim());
+    }
+}
